5c8c96f52137e5e83644d82dd35a28327ea065ad
aman_urmi <muhammadamanullah6001@gmail.com>
2024-03-02 22:23:19 +0600

    first commit

---
+++ true-false.js
@@ -0,0 +1,23 @@
+/* False value:
+0
+""
+undefined
+null
+NaN
+false
+
+
+True Value:
+" ", "0", [], {}, "false"
+
+*/
+
+const name = false;
+
+if(name){
+    console.log('Condition is true');
+}
+
+else{
+    console.log('Condition is false');
+}
